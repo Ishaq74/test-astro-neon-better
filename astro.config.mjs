9626d55e28c0f8d64e8e7aa3a3aa68a4e8094786
Ishaq74 <ishaq.achour@gmail.com>
2025-09-07 18:36:42 +0200

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [icon()],
+  adapter: vercel(),
 });
